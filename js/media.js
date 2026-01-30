8416d5ff8b634142c863d07809bbc7df5ed4a80c
DIX <diana.tuzova140201@gmail.com>
2026-01-31 05:22:27 +0600

    Update media.js

--- js/media.js
+++ js/media.js
@@ -1,23 +1,24 @@
-export const fileToBase64 = file =>
-  new Promise((res, rej) => {
-    const reader = new FileReader();
-    reader.onload = () => res(reader.result);
-    reader.onerror = rej;
-    reader.readAsDataURL(file);
-  });
-
 export const renderMedia = (container, media, removeFn) => {
   container.innerHTML = '';
+
   media.forEach((m, i) => {
     const div = document.createElement('div');
     div.className = 'media-item';
+
+    const url = URL.createObjectURL(m.blob);
+
     div.innerHTML = `
       ${m.type.startsWith('image')
-        ? `<img src="${m.url}">`
-        : `<video src="${m.url}" controls></video>`}
+        ? `<img src="${url}">`
+        : `<video src="${url}" controls></video>`}
       <button>✖</button>
     `;
-    div.querySelector('button').onclick = () => removeFn(i);
+
+    div.querySelector('button').onclick = () => {
+      URL.revokeObjectURL(url);
+      removeFn(i);
+    };
+
     container.appendChild(div);
   });
 };
